02338d854640f00e314e6229298a6c426bb26ce1
Casey Guarasci <60052312+cguarasci@users.noreply.github.com>
2023-06-14 04:29:28 -0400

    Testing commits straight from Git (comment)

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
           <Route path="/one" element={<Home />} />
           <Route path="/two" element={<Home />} />
           <Route path="/three" element={<Home />} />
-          {/* Additional routes go here */}
         </Routes>
       </Router>
     );
